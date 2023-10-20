00add9245e51dc9019e9011fb8553bf018159fac
aarchi <117932380+aarchimodi@users.noreply.github.com>
2023-10-20 23:52:14 +0530

    external js that runs only in node.js

--- hello world.js
+++ hello world.js
@@ -1,5 +1,8 @@
-console.log("hello world") 
+console.log("This is js") ;
+// even if u attach external js with html page it is still not printing on web
+// console.log works in node and console tab and not on web
 
 
-alert("hello world");
-alert("hi");
+alert("namste");
+//on web namste is popping up 1st
+
